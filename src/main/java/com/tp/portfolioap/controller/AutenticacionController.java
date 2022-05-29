36b31b7d90ecf4e959e0216e1bc47aa9857e1514
Minhi010 <lconlyentretenim@gmail.com>
2022-05-29 20:08:41 -0300

    controller auth

--- src/main/java/com/tp/portfolioap/controller/AutenticacionController.java
+++ src/main/java/com/tp/portfolioap/controller/AutenticacionController.java
@@ -1,5 +1,30 @@
 package com.tp.portfolioap.controller;
 
-public class AutenticacionController {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.tp.portfolioap.model.UsuarioDTO;
 
+@RestController
+@RequestMapping("/auth")
+public class AutenticacionController {
+	/*Login de paso, falto implementar jwt por falta de tiempo :( */
+	private final String USUARIO="lauc";
+	private final String CONTRASEÑA="PortfolioAP";
+
+	
+	@PostMapping("/login")
+	public ResponseEntity<Boolean> login(@RequestBody UsuarioDTO usuarioDTO ){
+		if(usuarioDTO.getUsuario() == null || usuarioDTO.getContraseña() == null) {
+			return new ResponseEntity<>(false, HttpStatus.BAD_REQUEST);	
+		}
+		if(usuarioDTO.getUsuario().toLowerCase().equals(USUARIO) && usuarioDTO.getContraseña().equals(CONTRASEÑA)) {
+		return new ResponseEntity<>(true, HttpStatus.OK);	
+		}
+		return new ResponseEntity<>(false, HttpStatus.UNAUTHORIZED);		
+	}
+	
 }
---
+++ src/main/java/com/tp/portfolioap/model/UsuarioDTO.java
@@ -0,0 +1,9 @@
+package com.tp.portfolioap.model;
+
+import lombok.Data;
+
+@Data
+public class UsuarioDTO {
+	private String usuario;
+	private String contraseña;
+}
